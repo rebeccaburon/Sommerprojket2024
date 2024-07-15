162a9534a46cb709a3fdaec2aac3d7e43dd8ec12
rebeccaburon <rebecca.mary.buron@hotmail.com>
2024-07-15 14:27:29 +0200

    Tilføjelse af MVC akitekturen og lidt i main

--- RootsForRecipes/src/main/java/app/Main.java
+++ RootsForRecipes/src/main/java/app/Main.java
@@ -1,7 +1,19 @@
 package app;
 
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Initializing Javalin and Jetty webserver
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        // Routing
     }
 }
